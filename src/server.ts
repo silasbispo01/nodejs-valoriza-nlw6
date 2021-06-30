c09bb38d753f8116579ee659b235fe3992562660
silasbispo01 <silasbispo01@gmail.com>
2021-06-30 09:51:22 -0300

    feat: create table + migrations

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import "express-async-errors";
 import "reflect-metadata";
 import "./database"
 import { router } from './routes';
@@ -8,6 +9,18 @@ const app = express();
 app.use(express.json())
 app.use(router);
 
+/// middleware de error, 4 parâmetros
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+})
 app.listen(3000, () => console.log('Server is running...'))
 
 
